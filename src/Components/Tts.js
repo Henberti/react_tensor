fbc0860bbe01c06d5da31691cfdde86114dec9dc
Henberti <hennnb@outlook.co.il>
2024-06-19 15:00:31 +0300

    adding logo

--- src/Components/Tts.js
+++ src/Components/Tts.js
@@ -4,7 +4,7 @@ const Tts = (message, voice, interrupt = false) => {
         window.speechSynthesis.cancel(); 
       }
       const utterance = new SpeechSynthesisUtterance(message);
-      utterance.voice = voice;
+      // utterance.voice = voice;
       window.speechSynthesis.speak(utterance);
     } else {
       console.log("Speech synthesis not supported.");
